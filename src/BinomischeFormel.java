9b07e9fe4a376bd760636856812b51c35dd242cf
14267 <vanessa.viehweger@nordakademie.de>
2015-10-20 10:55:52 +0200

    :)

---
+++ src/BinomischeFormel.java
@@ -0,0 +1,16 @@
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double a = 8;
+double b = 3;
+
+double loesung = a*a +2*a*b+ (b*b);
+
+System.out.println("(a+b)^2 = " + loesung);
+
+
+	}
+
+}
